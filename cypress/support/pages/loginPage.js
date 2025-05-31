1732649542637120fca1a2e2c3a7fbe3e01b21f1
I.Harlow <postbox382@gmail.com>
2025-05-31 13:03:53 +0530

    Updated workspace settings and disabled environment approval requirement in YAML configuration

--- cypress/support/pages/loginPage.js
+++ cypress/support/pages/loginPage.js
@@ -1,9 +1,12 @@
+import { When } from '@badeball/cypress-cucumber-preprocessor';
+
 export class LoginPage {
   elements = {
     username: () => cy.get('[name="username"]'),
     password: () => cy.get('[name="password"]'),
     submit: () => cy.get('button[type="submit"]'),
     error: () => cy.get('.oxd-alert-content-text'),
+    fieldValidationError: () => cy.get('.oxd-input-group__message'),
   };
 
   login(username, password) {
